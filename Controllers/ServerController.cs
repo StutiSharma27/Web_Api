3716b1b3eff77113090814cf12124af0270acd75
StutiSharma27 <92028077+StutiSharma27@users.noreply.github.com>
2023-12-29 15:39:27 +0530

    Add files via upload

---
+++ Controllers/ServerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Galytix.WebApi.Controllers
+{
+    public class ServerController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/gwp/avg")]
+        public IActionResult CalculateAverageGwp([FromBody] JObject requestData)
+        {
+            try
+            {
+                string country = requestData["country"].ToString();
+                List<string> lobs = requestData["lob"].ToObject<List<string>>();
+
+                Dictionary<string, double> result = CalculateAverageForCountry(country, lobs);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    private Dictionary<string, double> CalculateAverageForCountry(string country, List<string> lobs)
+{
+    string csvFilePath = Path.Combine("Data", "gwpByCountry.csv");
+
+    var data = System.IO.File.ReadLines(csvFilePath)
+        .Skip(1) // Skip header
+        .Select(line => line.Split(','))
+        .Where(parts => parts.Length >= 6 && parts[0].ToLower() == country.ToLower() && lobs.Contains(parts[3].ToLower()))
+        .SelectMany(parts => parts.Skip(4).Where(s => !string.IsNullOrEmpty(s)).Select(s => double.TryParse(s, out double value) ? value : 0.0)) // Assuming the GWP values start from index 4
+        .ToList();
+
+    var result = lobs.ToDictionary(
+        lob => lob,
+        lob => data.Where((_, index) => index % lobs.Count == lobs.IndexOf(lob)).Average()
+    );
+
+    return result;
+}
+    }
+}
